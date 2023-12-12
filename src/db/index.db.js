d94f9479d1917369f3cd550fc751b83dc417dfd0
niteshsainicoder <kumarsundratijara@gmail.com>
2023-12-12 20:45:28 +0530

    some new function written in db.js  file

--- src/db/index.db.js
+++ src/db/index.db.js
@@ -2,11 +2,19 @@ import mongoose, { connect } from "mongoose"
 
 const dbconnect = async ()=>
 {
-    const connect = await mongoose.connect(`${process.env.MONGO_URL}`).then(()=>{
+    mongoose.connection.on('connected', () => console.log('connected'));
+mongoose.connection.on('open', () => console.log('open'));
+mongoose.connection.on('disconnected', () => console.log('disconnected'));
+mongoose.connection.on('reconnected', () => console.log('reconnected'));
+mongoose.connection.on('disconnecting', () => console.log('disconnecting'));
+mongoose.connection.on('close', () => console.log('close'));
+
+    const connect = await mongoose.connect(`${process.env.MONGO_URL}`,).then(()=>{
 console.log(` the server is connect:`)
     }).catch((err)=>{
 console.log(   `the error is : ${err}`);
     })
+    
     return connect;
 }
 
